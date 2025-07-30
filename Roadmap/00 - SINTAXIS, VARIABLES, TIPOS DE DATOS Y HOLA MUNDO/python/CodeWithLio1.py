a91620d62e6d345930d6852005eaf1d93d202780
CodeWithLio1 <echeverrialionel1@gmail.com>
2025-07-30 18:26:58 -0300

    00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/CodeWithLio1.py
@@ -0,0 +1,30 @@
+# https://www.python.org/
+
+# Un sola linea
+
+'''
+Tdo lo que escriba aquí
+será parte del comentario.
+'''
+
+variable = 'Texto'
+
+constante_num = 1
+
+# En python no existen, pero.. existe:
+CONSTANTE = 'Constante fake'
+
+# Datos primitivos
+primitivos0 = int(1) # Ingresa Números enteros
+primitivos1 = float(1.5)# Ingresa Números decimales
+primitivos2 = True # Booleano puede ser Verdadero o Falso
+primitivos3 = 'Texto' # Str
+
+print(
+    '¡Hola, Python !'
+)
+
+print(type(primitivos0))
+print(type(primitivos1))
+print(type(primitivos2))
+print(type(primitivos3))
